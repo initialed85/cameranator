6178bcd8724abce76d7c1e1cc08c4f497b6d4c0f
Edward Beech <initialed85@gmail.com>
2021-01-03 10:20:01 +0800

    Pull some of the logic of the app out into it's own object

--- front_end/src/index.tsx
+++ front_end/src/index.tsx
@@ -1,8 +1,70 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/app/app";
+import { AppModel } from "./components/app/app_logic";
+import { AppProps } from "./components/app/app_props";
+import { Alert, Container, Row, Spinner } from "react-bootstrap";
 
-// Importing the Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
+new AppModel((props) => {
+    ReactDOM.render(<App {...props} />, document.getElementById("root"));
+});
 
-ReactDOM.render(<App />, document.getElementById("root"));
+interface IndexState {
+    appProps: AppProps | null;
+}
+
+class Index extends React.Component<any, IndexState> {
+    mounted: boolean;
+    appModel: AppModel;
+
+    constructor(props: any, state: IndexState) {
+        super(props, state);
+
+        this.state = {
+            appProps: null,
+        };
+
+        this.mounted = false;
+        this.appModel = new AppModel((appProps) => {
+            this.appUpdateHandler(appProps);
+        });
+    }
+
+    appUpdateHandler(appProps: AppProps) {
+        if (!this.mounted) {
+            return;
+        }
+
+        this.setState({ appProps: appProps });
+    }
+
+    componentDidMount() {
+        this.mounted = true;
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+    }
+
+    render() {
+        if (this.state.appProps == null) {
+            return (
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Alert variant={"info"}>Loading, please wait...</Alert>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                    </Row>
+                </Container>
+            );
+        }
+
+        return <App {...this.state.appProps} />;
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
--- front_end/src/persistence/utils.ts
+++ front_end/src/persistence/utils.ts
@@ -1,5 +1,5 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
-import {uri} from "../config";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { uri } from "../config/config";
 
 export function getClient() {
     return new ApolloClient({
