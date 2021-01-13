6add790672ff84b62a4a51afc933cf4d5ef9ddcf
Edward Beech <initialed85@gmail.com>
2021-01-13 22:46:10 +0800

    Added event pruner to keep DB size down

---
+++ cmd/event_pruner/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/initialed85/cameranator/pkg/services/event_pruner"
+	"github.com/initialed85/cameranator/pkg/utils"
+)
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+
+	urlFlag := flag.String("url", "http://localhost:8080/v1/graphql", "")
+	timeoutFlag := flag.Duration("timeout", time.Second*30, "")
+	intervalFlag := flag.Duration("interval", time.Second*5, "")
+
+	flag.Parse()
+
+	url := *urlFlag
+	timeout := *timeoutFlag
+	interval := *intervalFlag
+
+	if url == "" || !strings.Contains(url, "http://") {
+		log.Fatal("invalid -url argument; must be HTTP URL for GraphQL instance")
+	}
+
+	if timeout <= time.Duration(0) {
+		log.Fatal("invalid -timeout argument; must be > 0s")
+	}
+
+	if interval <= time.Duration(0) {
+		log.Fatal("invalid -interval argument; must be > 0s")
+	}
+
+	eventPruner, err := event_pruner.NewEventPruner(url, timeout, interval)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	eventPruner.Start()
+	log.Printf("Press Ctrl + C to exit...")
+	utils.WaitForCtrlC()
+	eventPruner.Stop()
+}
---
+++ pkg/services/event_pruner/event_pruner.go
@@ -0,0 +1,92 @@
+package event_pruner
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/initialed85/glue/pkg/worker"
+
+	"github.com/initialed85/cameranator/pkg/persistence/application"
+	"github.com/initialed85/cameranator/pkg/persistence/model"
+)
+
+type EventPruner struct {
+	scheduledWorker *worker.ScheduledWorker
+	application     *application.Application
+}
+
+func NewEventPruner(
+	url string,
+	timeout time.Duration,
+	interval time.Duration,
+) (*EventPruner, error) {
+	var err error
+
+	e := EventPruner{}
+
+	e.scheduledWorker = worker.NewScheduledWorker(
+		func() {},
+		e.work,
+		func() {},
+		interval,
+	)
+
+	e.application, err = application.NewApplication(url, timeout)
+
+	return &e, err
+}
+
+func (e *EventPruner) work() {
+	eventModel, err := e.application.GetModelAndClient("event")
+	if err != nil {
+		log.Printf("warning: %v", err)
+		return
+	}
+
+	events := make([]model.Event, 0)
+
+	err = eventModel.GetAll(&events)
+	if err != nil {
+		log.Printf("warning: %v", err)
+		return
+	}
+
+	for _, event := range events {
+		paths := make([]string, 0)
+
+		paths = append(paths, event.HighQualityVideo.FilePath)
+		paths = append(paths, event.LowQualityVideo.FilePath)
+		paths = append(paths, event.HighQualityImage.FilePath)
+		paths = append(paths, event.LowQualityImage.FilePath)
+
+		remove := true
+		for _, path := range paths {
+			_, err := os.Stat(path)
+
+			if err == nil {
+				remove = false
+				break
+			}
+		}
+
+		if !remove {
+			continue
+		}
+
+		log.Printf("attempting to delete %#+v", event)
+		err = eventModel.Remove(event, &[]model.Event{})
+		if err != nil {
+			log.Printf("warning: %v", err)
+			continue
+		}
+	}
+}
+
+func (e *EventPruner) Start() {
+	e.scheduledWorker.Start()
+}
+
+func (e *EventPruner) Stop() {
+	e.scheduledWorker.Stop()
+}
